217827130c39d8ad007bb370916929969955bcea
Lololemil <leko@riseup.net>
2015-09-01 19:21:10 +0200

    write to filw after each question i Please enter the cemmit message for your changes. Lines starting

--- remind.py
+++ remind.py
@@ -10,20 +10,16 @@ from datetime import date, timedelta, datetime
 questionfile = 'questions.txt'
 txt = open(questionfile).readlines()
 stats = txt[0]
-
-# get the stats
-print(stats)
 if stats == '\n':
     stats = 'MemScore: 0 average_cpm: 100 Answered: 0'
+print(stats)
 stats = stats.split()
-
-# make the relevant elements of stats int
-stats[1] = int(stats[1])  # Total score
-stats[3] = int(stats[3])  # Average Characters per minute
-stats[5] = int(stats[5])  # Total answered right and above average cpm
 
 # Iterate through the lines in txt, bar the first stats line
 for i in range(1, len(txt)):
+    stats[1] = int(stats[1])  # Total score
+    stats[3] = int(stats[3])  # Average Characters per minute
+    stats[5] = int(stats[5])  # Total answered right and above average cpm
     line = txt[i].strip().split('###')
     if len(line) < 3:
         line.append(str(date.today()))
@@ -38,7 +34,7 @@ for i in range(1, len(txt)):
             print('Good: %s characters per minute' % cpm)
             score = int(line[3]) + 1
             if cpm > stats[3]:
-                print('Bonus for great speed! +3')
+                print('Bonus for great speed! +1')
                 stats[1] += 1
                 score += 1
             score += 1
@@ -51,15 +47,14 @@ for i in range(1, len(txt)):
         line[2] = str(date.today() + timedelta(days=score*2))
         line[3] = str(score)
         txt[i] = '###'.join(line) + '\n'
-
-# make the relevant elements of stats string
-stats[1] = str(stats[1])
-stats[3] = str(stats[3])
-stats[5] = str(stats[5])
-txt[0] = ' '.join(stats) + '\n'
-# Write to file
-writefile = open(questionfile, 'w')
-writefile.writelines(txt)
+    # make the relevant elements of stats string
+    stats[1] = str(stats[1])
+    stats[3] = str(stats[3])
+    stats[5] = str(stats[5])
+    txt[0] = ' '.join(stats) + '\n'
+    # Write to file
+    writefile = open(questionfile, 'w')
+    writefile.writelines(txt)
 
 print(txt[0])
 print("You are good!")
