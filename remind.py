add703abb3b36b7b7d4966ef9c92c558789a762d
Lololemil <leko@riseup.net>
2015-09-08 04:49:50 +0200

    python evaluation

--- remind.py
+++ remind.py
@@ -23,7 +23,24 @@ totalscore = int(stats[1])  # Total score
 averagecpm = int(stats[3])  # Average Characters per minute
 totalanswered = int(stats[5])  # Total answered right
 
-# Iterate through the lines in txt, bar the first stats line
+
+def goodanswer():
+    global timer, score, totalscore, averagecpm, totalanswered
+    # Calcultate characters per minute
+    cpm = int(len(answer)/(time()-timer)*60)
+    print('Good: %s characters per minute' % cpm)
+    # Scorebonus dependent on speed
+    scorebonus = int(cpm/averagecpm)
+    score = score + scorebonus + 1
+    print('Speed bonus: %s ' % scorebonus)
+    totalanswered = totalanswered + 1
+    # calculate new average cpm
+    averagecpm = int((averagecpm*(totalanswered-1)+cpm)/totalanswered)
+    # Update score
+    totalscore += scorebonus
+
+
+# Go through the lines in txt, bar the first stats line
 for i in range(1, len(txt)):
     # split the line and make a line list
     line = txt[i].strip().split('###')
@@ -33,7 +50,7 @@ for i in range(1, len(txt)):
         line.append('0')
     # Make the line items workable
     question = line[0]
-    rightanswer = line[1]
+    ranswer = line[1]
     date = datetime.strptime(line[2], '%Y-%m-%d').date()  # make date object
     score = int(line[3])
 
@@ -42,23 +59,20 @@ for i in range(1, len(txt)):
         print(question)
         timer = time()
         answer = input()
-        if answer.lower() == rightanswer.lower():  # check answer no matter caps
-            # Calcultate characters per minute
-            cpm = int(len(answer)/(time()-timer)*60)
-            print('Good: %s characters per minute' % cpm)
-            # Scorebonus dependent on speed
-            scorebonus = int(cpm/averagecpm)
-            score = score + scorebonus + 1
-            print('Speed bonus: %s ' % scorebonus)
-            totalanswered = totalanswered + 1
-            # calculate new average cpm
-            averagecpm = int((averagecpm*(totalanswered-1)+cpm)/totalanswered)
-            # Update score
-            totalscore += scorebonus
+
+        if answer.lower() == ranswer.lower():
+            goodanswer()
+        elif 'Python' in question:
+            try:
+                if eval(answer) == eval(ranswer):
+                    print('good python, god will reward you')
+                    goodanswer()
+            except SyntaxError:
+                pass
         else:
             score = score - 1
-            print("Better: " + rightanswer)
-            input("Kikkaki ")
+            print(ranswer)
+            input("Maybe some practice will help? ")
         line[2] = str(date.today() + timedelta(days=score))
         line[3] = str(score)
         txt[i] = '###'.join(line) + '\n'
